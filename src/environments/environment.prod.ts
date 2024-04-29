28d4a779497657453d2d7b6303a5b5e8e6c07e77
Vinodh <vinodhanu007@gmail.com>
2024-04-29 15:48:43 +0530

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const baseURL = 'http://localhost:3000/api/';
+
 export const environment = {
-  production: true
+  production: true,
+
+  inserEmpDetails: baseURL + 'test/Insert-emp-details',
+
+  EmpDetails: baseURL + 'test/employee-data',
 };
